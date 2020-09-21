21d92ed6967b4dc3668170276bdfd19d57e02f16
ito <sakiito0000@gmail.com>
2020-09-21 19:38:38 +0900

    API通信の機能テスト学習

--- laravel/tests/Feature/Http/Controllers/Api/Lesson/ReserveControllerTest.php
+++ laravel/tests/Feature/Http/Controllers/Api/Lesson/ReserveControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\Lesson;
+namespace Tests\Feature\Http\Controllers\Api\Lesson;
 
 use App\Models\Lesson;
 use App\Models\Reservation;
@@ -10,44 +10,50 @@ use Illuminate\Http\Response;
 use Tests\Factories\Traits\CreateUser;
 use Tests\TestCase;
 
+/**
+ * SPA想定の機能テスト
+ */
 class ReserveControllerTest extends TestCase
 {
-    use CreateUser;
     use RefreshDatabase;
+    use CreateUser;
 
-    public function testInvoke()
+
+    public function testInvoke_正常()
     {
         $lesson = factory(Lesson::class)->create();
-        $user   = $this->createUser();
-        $this->actingAs($user);
+        $user = $this->createUser();
+        $this->actingAs($user, 'api');
 
-        $response = $this->post("/lessons/{$lesson->id}/reserve");
+        $response = $this->postJson("/api/lessons/{$lesson->id}/reserve");
+        $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertJson([
+            'user_id'   => $user->id,
+            'lesson_id' => $lesson->id
+        ]);
 
-        $response->assertStatus(Response::HTTP_FOUND);
-        $response->assertRedirect("lessons/{$lesson->id}");
+        $this->assertDatabaseHas('reservations', [
+            'user_id'   => $user->id,
+            'lesson_id' => $lesson->id
+        ]);
     }
 
-    public function testInvoke_異常系()
+    public function testInvoke_異常()
     {
-        // 残り許容量が１枠のみのレッスン(他ユーザーが予約済み)を作成
         $lesson = factory(Lesson::class)->create(['capacity' => 1]);
         $anotherUser = $this->createUser();
         $lesson->reservations()->save(factory(Reservation::class)->make([
             'user_id'   => $anotherUser->id,
             'lesson_id' => $lesson->id
         ]));
-
         $user = $this->createUser();
-        $this->actingAs($user);
+        $this->actingAs($user, 'api');
 
-        $response = $this->from("/lessons/{$lesson->id}")
-            ->post("/lessons/{$lesson->id}/reserve");
+        $response = $this->postJson("/api/lessons/{$lesson->id}/reserve");
+        $response->assertStatus(Response::HTTP_CONFLICT);
 
-        $response->assertStatus(Response::HTTP_FOUND);
-        $response->assertRedirect("lessons/{$lesson->id}");
-
-        $response->assertSessionHasErrors();
-        $error = session('error')->first();
+        $response->assertJsonStructure(['error']);
+        $error = $response->json('error');
         $this->assertStringContainsString('予約できません。', $error);
 
         $this->assertDatabaseMissing('reservations', [
